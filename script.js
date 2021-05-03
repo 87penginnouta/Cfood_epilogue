9209675ea30c1ed2b8d5d88902734694cbde8805
87penginnouta <49667189+87penginnouta@users.noreply.github.com>
2021-05-04 06:58:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
+/*-----loading-----*/
+
+$(window).on("load", function () {
+  $('html').css('overflow-y', 'auto');
+  $(".loading_wrapper").fadeOut("slow");
+  $("#bg").css('visibility', 'visible');
+});
+
 var jq = $;
 
 AOS.init();
 
 
 /*-----錨點-----*/
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
+$(function () {
+  $('a[href*="#"]:not([href="#"])').click(function () {
     if (
       location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $("html, body").animate(
-          {
+        $("html, body").animate({
             scrollTop: target.offset().top
           },
           1000
@@ -28,48 +35,50 @@ $(function() {
 
 /*-----返回頂端-----*/
 
-$(function() {
-    /* 按下GoTop按鈕時的事件 */
-    $('#gotop').click(function(){
-        $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
-        return false;
-    });
-    
-    /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() > 500){
-            $('#gotop').fadeIn();
-        } else {
-            $('#gotop').fadeOut();
-        }
-    });
+$(function () {
+  /* 按下GoTop按鈕時的事件 */
+  $('#gotop').click(function () {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 'slow'); /* 返回到最頂上 */
+    return false;
+  });
+
+  /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $('#gotop').fadeIn();
+    } else {
+      $('#gotop').fadeOut();
+    }
+  });
 });
 
-$(document).ready(function() {
-	
-	// INITIATE THE FOOTER
+$(document).ready(function () {
+
+  // INITIATE THE FOOTER
   siteFooter();
-	// COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
-	$(window).resize(function() {
-		siteFooter();
-	});
-	
-	function siteFooter() {
-		var siteContent = $('#bg');
-		var siteContentHeight = siteContent.height();
-		var siteContentWidth = siteContent.width();
+  // COULD BE SIMPLIFIED FOR THIS PEN BUT I WANT TO MAKE IT AS EASY TO PUT INTO YOUR SITE AS POSSIBLE
+  $(window).resize(function () {
+    siteFooter();
+  });
 
-		var siteFooter = $('#footer');
-		var siteFooterHeight = siteFooter.height();
-		var siteFooterWidth = siteFooter.width();
+  function siteFooter() {
+    var siteContent = $('#bg');
+    var siteContentHeight = siteContent.height();
+    var siteContentWidth = siteContent.width();
 
-		console.log('Content Height = ' + siteContentHeight + 'px');
-		console.log('Content Width = ' + siteContentWidth + 'px');
-		console.log('Footer Height = ' + siteFooterHeight + 'px');
-		console.log('Footer Width = ' + siteFooterWidth + 'px');
+    var siteFooter = $('#footer');
+    var siteFooterHeight = siteFooter.height();
+    var siteFooterWidth = siteFooter.width();
 
-		siteContent.css({
-			"margin-bottom" : siteFooterHeight + 50
-		});
-	}
+    console.log('Content Height = ' + siteContentHeight + 'px');
+    console.log('Content Width = ' + siteContentWidth + 'px');
+    console.log('Footer Height = ' + siteFooterHeight + 'px');
+    console.log('Footer Width = ' + siteFooterWidth + 'px');
+
+    siteContent.css({
+      "margin-bottom": siteFooterHeight + 50
+    });
+  }
 });
